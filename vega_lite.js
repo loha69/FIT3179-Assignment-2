59d5f432416125c006ffa5fae865fd79df5c3984
loha69 <81837911+loha69@users.noreply.github.com>
2024-10-08 23:43:00 +1100

    Add files via upload

---
+++ vega_lite.js
@@ -0,0 +1,5 @@
+var vg_1 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/1CurrentIndustryPercentage.vg.json";
+vegaEmbed("#pie_chart", vg_1).then(function(result){}).catch(console.error);
+
+var vg_2 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/stacked_area_chart.vg.json";
+vegaEmbed("#stack_chart", vg_2).then(function(result){}).catch(console.error);
