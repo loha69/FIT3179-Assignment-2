b63595e9d0e09cb9fc342fe84412517914b26795
loha69 <81837911+loha69@users.noreply.github.com>
2024-10-15 16:54:15 +1100

    all graphs and style added, need to fix vega_lite

--- vega_lite.js
+++ vega_lite.js
@@ -1,17 +1,11 @@
-var vg_1 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/2CurrentIndustryPercentage.vg.json";
-vegaEmbed("#pie_chart", vg_1).then(function(result){}).catch(console.error);
+var vg_1 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Multilinechart.vg.json";
+vegaEmbed("#multi_chart", vg_1).then(function(result){}).catch(console.error);
 
-var vg_2 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/stacked_area_chart.vg.json";
-vegaEmbed("#stack_chart", vg_2).then(function(result){}).catch(console.error);
+var vg_2 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Map.vg.json";
+vegaEmbed("#map_chart", vg_2).then(function(result){}).catch(console.error);
 
-var vg_3 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Multilinechart.vg.json";
-vegaEmbed("#multi_chart", vg_3).then(function(result){}).catch(console.error);
+var vg_3 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Trendline.vg.json";
+vegaEmbed("#Trendline_chart", vg_3).then(function(result){}).catch(console.error);
 
-var vg_4 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/bar.vg.json";
-vegaEmbed("#bar_chart", vg_4).then(function(result){}).catch(console.error);
-
-var vg_5 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Map.vg.json";
-vegaEmbed("#map_chart", vg_5).then(function(result){}).catch(console.error);
-
-var vg_6 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Trendline.vg.json";
-vegaEmbed("#Trendline_chart", vg_6).then(function(result){}).catch(console.error);
+var vg_4 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/combined.vg.json";
+vegaEmbed("#combine_chart", vg_4).then(function(result){}).catch(console.error);
