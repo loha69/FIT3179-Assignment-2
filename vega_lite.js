bc143c6df6ee674db7ef7694a4374ab8b8ee4315
loha69 <81837911+loha69@users.noreply.github.com>
2024-10-15 18:25:14 +1100

    Add files via upload

--- vega_lite.js
+++ vega_lite.js
@@ -1,11 +1,8 @@
-var vg_1 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Multilinechart.vg.json";
-vegaEmbed("#multi_chart", vg_1).then(function(result){}).catch(console.error);
+var vg_1 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Brushing.vg.json";
+vegaEmbed("#brushing_chart", vg_1).then(function(result){}).catch(console.error);
 
-var vg_2 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Map.vg.json";
-vegaEmbed("#map_chart", vg_2).then(function(result){}).catch(console.error);
+var vg_2 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Trendline.vg.json";
+vegaEmbed("#Trendline_chart", vg_2).then(function(result){}).catch(console.error);
 
-var vg_3 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/Trendline.vg.json";
-vegaEmbed("#Trendline_chart", vg_3).then(function(result){}).catch(console.error);
-
-var vg_4 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/combined.vg.json";
-vegaEmbed("#combine_chart", vg_4).then(function(result){}).catch(console.error);
+var vg_3 = "https://raw.githubusercontent.com/loha69/FIT3179-Assignment-2/refs/heads/main/combined.vg.json";
+vegaEmbed("#combine_chart", vg_3).then(function(result){}).catch(console.error);
